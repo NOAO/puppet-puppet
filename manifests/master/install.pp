dcc54e5497ac1fa973a9b331d68a19b031cba3b0
Joshua Hoblitt <jhoblitt@cpan.org>
2014-11-25 14:14:09 -0700

    fix typo of $puppet_master_package_name

--- manifests/master/install.pp
+++ manifests/master/install.pp
@@ -1,7 +1,7 @@
 class puppet::master::install {
 
   if $::puppet::master::puppet_master_package_name {
-    package { $p::puppet::master::uppet_master_package_name:
+    package { $::puppet::master::puppet_master_package_name:
       ensure => present,
     }
   }
